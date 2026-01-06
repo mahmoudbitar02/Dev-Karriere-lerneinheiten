16a7cc8e3d35fddf0eec159be2af8dfe1072cad4
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2026-01-06 19:38:22 +0100

    try to add some js code

--- Modul-03/Chapter-05/script.js
+++ Modul-03/Chapter-05/script.js
@@ -1,3 +1,16 @@
-function addNumber() {
-  console.log("hello");
-}
+// const calc = document.getElementById("calc");
+// const current = document.getElementById("current");
+// let test;
+
+// function addValue(digit) {
+//   test = current.innerHTML += digit;
+// }
+
+// function calculat() {
+//   console.log(test);
+//   calc.innerHTML = eval(test);
+// }
+
+// function clear() {
+//   current.innerHTML = 0;
+// }
