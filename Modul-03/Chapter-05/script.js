7b0b4da0f63df78fb973f027d4889020f058f51a
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2026-01-07 21:27:14 +0100

    finish pizza mamma mia

--- Modul-03/Chapter-05/script.js
+++ Modul-03/Chapter-05/script.js
@@ -1,16 +1,1 @@
-// const calc = document.getElementById("calc");
-// const current = document.getElementById("current");
-// let test;
 
-// function addValue(digit) {
-//   test = current.innerHTML += digit;
-// }
-
-// function calculat() {
-//   console.log(test);
-//   calc.innerHTML = eval(test);
-// }
-
-// function clear() {
-//   current.innerHTML = 0;
-// }
