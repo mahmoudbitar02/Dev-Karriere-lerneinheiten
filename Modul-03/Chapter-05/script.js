bc6c35c0757bed3fcc5551655cbd3c4a56253a67
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2026-01-08 22:12:36 +0100

    try to create calculator js

--- Modul-03/Chapter-05/script.js
+++ Modul-03/Chapter-05/script.js
@@ -1,1 +1,69 @@
+let currentValue = "0";
+let currentOperator = "";
+let prevValue = 0;
 
+const current = document.getElementById("current");
+const calc = document.getElementById("calc");
+
+function addValue(number) {
+  currentValue = number;
+  current.innerHTML = currentValue;
+}
+
+function addOperator(operator) {
+  if (currentValue === "") return;
+
+  prevValue = calc.innerHTML;
+
+  calc.innerHTML = parseInt(prevValue) + parseInt(currentValue) + operator;
+
+  current.innerHTML = "0";
+}
+
+// function addOperator(operator) {
+//   if (currentValue === "") return;
+//   if (prevValue !== "") calculat();
+//   prevValue = currentValue;
+//   currentValue = "0";
+//   currentOperator = operator;
+
+//   test.innerHTML = prevValue + currentOperator + currentValue;
+// }
+
+function calculat() {
+  switch (currentOperator) {
+    case "+":
+      calc.innerHTML = parseInt(prevValue) + parseInt(currentValue);
+      break;
+
+    case "-":
+      calc.innerHTML = parseInt(prevValue) - parseInt(currentValue);
+      break;
+
+    case "*":
+      calc.innerHTML = parseInt(prevValue) * parseInt(currentValue);
+      break;
+
+    case "/":
+      if (currentValue === "0") {
+        alert("can't devidy by zero");
+        return;
+      }
+      calc.innerHTML = parseInt(prevValue) / parseInt(currentValue);
+      break;
+
+    default:
+      break;
+  }
+  console.log("trest");
+}
+
+function clearValue() {
+  currentValue = "";
+  prevValue = "";
+  currentOperator = "";
+  current.innerHTML = "0";
+  calc.innerHTML = "0";
+
+  console.log("trest");
+}
