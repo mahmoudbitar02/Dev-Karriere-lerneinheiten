83e6018845975d5c98da2563d1469914f4e7cd45
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2026-01-25 18:18:02 +0100

    finish weiter projekt

--- Modul-04/Chapter-02/Aufgabe-01/weitere-Aufgabe/script.js
+++ Modul-04/Chapter-02/Aufgabe-01/weitere-Aufgabe/script.js
@@ -12,9 +12,30 @@ function createObject() {
       deutsch: document.getElementById("deutsch").value,
     },
   };
+
+  for (const scoreKey in student.score) {
+    if (student.score[scoreKey] === "") {
+      student.score[scoreKey] = 5;
+    } else if (student.score[scoreKey] < 1 || student.score[scoreKey] > 6) {
+      alert(`Ungültiger Wert für ${scoreKey}. Bitte geben Sie eine Zahl zwischen 1 und 6 ein.`);
+      return;
+    } else {
+      student.score[scoreKey] = Number(student.score[scoreKey]);
+      console.log(student.score[scoreKey]);
+    }
+  }
+
+  if (student.vorName.length < 2) {
+    alert("Vorname muss mindestens 2 Zeichen lang sein.");
+    return;
+  } else if (student.alter < 6) {
+    alert("Alter muss mindestens 6 Jahre sein.");
+    return;
+  }
   const index = students.findIndex((studentsInListe) => {
     return studentsInListe.vorName === student.vorName && studentsInListe.nachName === student.nachName && studentsInListe.alter === student.alter;
   });
+
   if (index !== -1) {
     students[index] = student;
     schowObject();
