6a088efe5159f8e4eddebf411b2f27e79478cc5d
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2026-01-28 19:57:09 +0100

    finish aufgabe 1

--- Modul-04/Chapter-03/Aufgabe-01/script.js
+++ Modul-04/Chapter-03/Aufgabe-01/script.js
@@ -1,44 +1,55 @@
 const students = [
   {
-    vorname: "mahmoud",
-    nachname: "bitar",
-    alter: 28,
+    Vorname: "Mahmoud",
+    Nachname: "Bitar",
+    Alter: 28,
   },
   {
-    vorname: "Torsten",
-    nachname: "Müller",
-    alter: 30,
+    Vorname: "Torsten",
+    Nachname: "Müller",
+    Alter: 30,
   },
   {
-    vorname: "Thomas",
-    nachname: "Schneider",
-    alter: 35,
+    Vorname: "Thomas",
+    Nachname: "Schneider",
+    Alter: 35,
   },
   {
-    vorname: "Jan",
-    nachname: "Meier",
-    alter: 25,
+    Vorname: "Jan",
+    Nachname: "Meier",
+    Alter: 25,
   },
   {
-    vorname: "Kay",
-    nachname: "Körner",
-    alter: 20,
+    Vorname: "Kay",
+    Nachname: "Körner",
+    Alter: 20,
   },
 ];
 
-const randmoStudent = students[Math.floor(Math.random() * students.length)];
-console.log(randmoStudent);
-
 function createStudent() {
+  const randmoStudent = students[Math.floor(Math.random() * students.length)];
+  console.log(randmoStudent);
+
   const card = document.createElement("div");
   card.classList.add("card");
+
+  for (const key in randmoStudent) {
+    // create p-tag and add the text
+    const paragraph = document.createElement("p");
+    const paragraphText = document.createTextNode(key); // key --> properates from the "students" object
+    paragraph.appendChild(paragraphText);
+
+    const span = document.createElement("span");
+    const spanText = document.createTextNode(`: ${randmoStudent[key]}`); // randmoStudent[key] --> the value from the
+    span.appendChild(spanText);
+
+    paragraph.appendChild(span);
+    console.log(paragraph);
+
+    card.appendChild(paragraph);
+  }
+
+  document.querySelector(".container").appendChild(card);
+
   console.log(card);
-
-  const paragraph = document.createElement("p");
-  const paragraphText = document.createTextNode("hello");
-  paragraph.appendChild(paragraphText);
-
-  const span = document.createElement("span");
-  paragraph.appendChild(span);
-  console.log(paragraph);
 }
