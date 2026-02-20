ae6b1248fd4c8dfea62a1176b210f153fa14516f
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2026-02-20 14:49:54 +0100

    work on notiz-app

--- Modul-05/Notiz-App/script.js
+++ Modul-05/Notiz-App/script.js
@@ -2,6 +2,7 @@ console.log("Notiz-App gestartet");
 
 const titleInput = document.getElementById("title");
 const textInput = document.getElementById("text");
+const sideBar = document.getElementById("wrapper");
 
 const saveBtn = document.getElementById("save");
 
@@ -10,15 +11,54 @@ const notes = [];
 function handelSaveBtn() {
   saveBtn.addEventListener("click", () => {
     const now = new Date();
-    const formatted = now.toLocaleTimeString("de-DE");
+    const formatted = now.toLocaleString("de-DE");
     const note = {
       title: titleInput.value,
       text: textInput.value,
       updateTime: formatted,
+      showed: false,
     };
 
     notes.push(note);
+    createNote();
+    titleInput.value = "";
+    textInput.value = "";
     console.log(notes);
   });
+}
+
+function createNote() {
+  notes.forEach((note) => {
+    if (!note.showed) {
+      const div = document.createElement("div");
+      div.classList.add("notes-card");
+      const title = document.createElement("h5");
+      title.classList.add("notes-title");
+      title.textContent = note.title;
+      const paragraph = document.createElement("p");
+      paragraph.classList.add("notes-text");
+      paragraph.textContent = note.text;
+      const timeSpan = document.createElement("span");
+      timeSpan.classList.add("notes-date");
+      timeSpan.textContent = note.updateTime;
+
+      div.appendChild(title);
+      div.appendChild(paragraph);
+      div.appendChild(timeSpan);
+
+      sideBar.prepend(div);
+      note.showed = true;
+      console.log(note);
+      div.addEventListener("click", () => {
+        handelNoteClick(note);
+      });
+    }
+  });
+}
+
+function handelNoteClick(note) {
+  titleInput.value = note.title;
+  textInput.value = note.text;
+  console.log(note);
 }
 handelSaveBtn();
