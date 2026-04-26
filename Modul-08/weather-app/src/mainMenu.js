7ab313cdd930d6e932732d22508cca876273caf2
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2026-04-26 21:50:48 +0200

    finish task 6

--- Modul-08/weather-app/src/mainMenu.js
+++ Modul-08/weather-app/src/mainMenu.js
@@ -1,17 +1,20 @@
+import { getForcastWeather } from "./api";
+import { getConditionImagePath } from "./conditions";
 import { loadDetailView } from "./detailView";
 import { renderLoadingScreen } from "./loading";
 import { rootElement } from "./main";
+import { formatTemperature } from "./utils";
 
-export function loadMainMenu() {
+export async function loadMainMenu() {
   rootElement.classList.remove("show-background");
   renderLoadingScreen("Lade Übersicht...");
-  renderMainMenu();
+  await renderMainMenu();
 }
-function renderMainMenu() {
+async function renderMainMenu() {
   rootElement.innerHTML = `
         <div class=main-menu">
         ${getMenuHeaderHtml()}
-        ${getCityListHtml()}
+        ${await getCityListHtml()}
 
         </div>
     `;
@@ -28,25 +31,33 @@ function getMenuHeaderHtml() {
     `;
 }
 
-function getCityListHtml() {
+async function getCityListHtml() {
   const favoriteCities = ["Mannheim", "London", "Peking"];
 
   const favoriteCityElement = [];
 
   for (let city of favoriteCities) {
+    const weatherData = await getForcastWeather(city, 1);
+    const { location, current, forecast } = weatherData;
+    const currentDay = forecast.forecastday[0];
+
+    const conditionImage = getConditionImagePath(current.condition.code, current.is_day !== 1);
+
     const cityHtml = `
         <div class="city-wrapper">
-            <div class="city" data-city-name="${city}" style="--condition-image: url(/weather-app/conditionImages/day/sunny.jpg)">
+            <div class="city" data-city-name="${location.name}" ${conditionImage ? `style="--condition-image: url(${conditionImage})"` : ""}>
               <div class="city__left-column">
-                <h2 class="city__name">${city}</h2>
-                <div class="city__country">Germany</div>
-                <div class="city__condition">Sonnig</div>
+                <h2 class="city__name">${location.name}</h2>
+                <div class="city__country">${location.country}</div>
+                <div class="city__condition">${current.condition.text}</div>
               </div>
               <div class="city__right-column">
-                <div class="city__temperature">20°</div>
-                <div class="city__min-max-temperature">H:21° T:11°</div>
+                <div class="city__temperature">${formatTemperature(current.temp_c)}°</div>
+                <div class="city__min-max-temperature">H:${formatTemperature(currentDay.day.maxtemp_c)}° T:${formatTemperature(currentDay.day.mintemp_c)}°</div>
               </div>
             </div>
+
+            
         </div>
     `;
 
@@ -65,7 +76,8 @@ function registerEventListeners() {
   const cities = document.querySelectorAll(".city");
   cities.forEach((city) => {
     city.addEventListener("click", () => {
-      const cityName = city.getAttribute("data.city-name");
+      const cityName = city.getAttribute("data-city-name");
+      console.log(cityName);
       loadDetailView(cityName);
     });
   });
