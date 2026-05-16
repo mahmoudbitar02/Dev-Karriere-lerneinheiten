4b1fa3f211766bc9336fd9307120d7a98e1a78e9
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2026-05-17 00:17:34 +0200

    finish chapter 03 React

--- Modul-09/chapter-03/hooks/src/App.js
+++ Modul-09/chapter-03/hooks/src/App.js
@@ -16,10 +16,36 @@ function Button() {
   );
 }
 
+function Button2() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <button onClick={() => setCount(count + 1)} type="">
+        {count}
+      </button>
+    </>
+  );
+}
+
+function Input() {
+  const [input, setInput] = useState("");
+  function handelInputChange(event) {
+    setInput(event.target.value);
+  }
+  return (
+    <>
+      <input onChange={handelInputChange} type="text" />
+      <p>{input}</p>
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <Button />
+      <Button2 />
+      <Input />
     </div>
   );
 }
